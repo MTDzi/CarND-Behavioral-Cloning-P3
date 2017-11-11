31504f119be4015dfa5cb2639509896cf55c87c5
M <ponadto@gmail.com>
2017-11-11 08:07:36 +0100

    Before pull

--- drive.py
+++ drive.py
@@ -16,10 +16,13 @@ from keras.models import load_model
 import h5py
 from keras import __version__ as keras_version
 
+from image_preprocessing import process_image
+
 sio = socketio.Server()
 app = Flask(__name__)
 model = None
 prev_image_array = None
+
 
 
 class SimplePIController:
@@ -61,6 +64,7 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
+        image_array = process_image(image_array)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
